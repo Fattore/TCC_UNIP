e0974623fdaf721d94aec49a9cc30f8bd6a660aa
Fratty <murilofattore@hotmail.com>
2022-07-05 21:47:55 -0300

    changes on screen

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.geometry('300x150')
-        self.resizable(0, 0)
-        self.title('Login')
+        self.resizable(False, False)
+        self.title('Sistema Especialista de Diagnóstico Médico')
 
         # UI options
         paddings = {'padx': 5, 'pady': 5}
@@ -19,26 +18,26 @@ class App(tk.Tk):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=3)
 
-        username = tk.StringVar()
-        password = tk.StringVar()
+        doencaum = tk.StringVar()
+        doencadois = tk.StringVar()
 
         # heading
         heading = ttk.Label(self, text='Sintomas', style='Heading.TLabel')
         heading.grid(column=0, row=0, columnspan=2, pady=5, sticky=tk.N)
 
-        # username
-        diseaseone_label = ttk.Label(self, text="Dor de cabeca:")
-        diseaseone_label.grid(column=0, row=1, sticky=tk.W, **paddings)
+        # doencaum
+        doencaum_label = ttk.Label(self, text="doencaum:")
+        doencaum_label.grid(column=0, row=1, sticky=tk.W, **paddings)
 
-        diseaseone_entry = ttk.Entry(self, textvariable=username, **entry_font)
-        diseaseone_entry.grid(column=1, row=1, sticky=tk.E, **paddings)
+        doencaum_entry = ttk.Entry(self, textvariable=doencaum, **entry_font)
+        doencaum_entry.grid(column=1, row=1, sticky=tk.E, **paddings)
+        
+		# doencaum
+        doencadois_label = ttk.Label(self, text="doencadois:")
+        doencadois_label.grid(column=0, row=2, sticky=tk.W, **paddings)
 
-		# username
-        diseasetwo_label = ttk.Label(self, text="Febre:")
-        diseasetwo_label.grid(column=0, row=2, sticky=tk.W, **paddings)
-
-        diseasetwo_entry = ttk.Entry(self, textvariable=username, **entry_font)
-        diseasetwo_entry.grid(column=1, row=2, sticky=tk.E, **paddings)
+        doencadois_entry = ttk.Entry(self, textvariable=doencadois, **entry_font)
+        doencadois_entry.grid(column=1, row=2, sticky=tk.E, **paddings)
 
         # login button
         login_button = ttk.Button(self, text="Login")
